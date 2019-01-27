a49f37d0e07ce8879fe7606887e8055db56cefc5
Rishi Badiani <rishib9000@hotmail.com>
2019-01-27 19:38:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index] == " " || ""
+    false
+  elsif board[index] == " "
+    false
+  else board[index] == nil
+    false 
+  end
+end
+  
